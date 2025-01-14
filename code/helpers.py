232c192c37350844d47c90ffe428f9b55c5686fc
MinAiSpaces <193891364+MinAiSpaces@users.noreply.github.com>
2025-01-14 15:15:26 +0100

    fix: add docstrings and typing to helper.py

--- code/helpers.py
+++ code/helpers.py
@@ -1,25 +1,40 @@
 import os
 
 
-def get_path(*args):
+def get_path(*args: str) -> str:
+    """
+    Helper function for getting the full path of a file or folder in this project
+    """
     current_dir = os.path.dirname(__file__)
 
-    return os.path.join(current_dir, *args)
+    return str(os.path.join(current_dir, *args))
 
 
-def get_data_path():
+def get_data_path() -> str:
+    """
+    Helper function for getting the full path of the 'data' folder in this project
+    """
     return get_path('..', 'data')
 
 
-def get_input_path():
+def get_input_path() -> str:
+    """
+    Helper function for getting the full path of the 'input' folder in this project
+    """
     return get_path('..', 'data', 'input')
 
 
-def get_output_path():
+def get_output_path() -> str:
+    """
+    Helper function for getting the full path of the 'output' folder in this project
+    """
     return get_path('..', 'data', 'output')
 
 
-def check_or_create_dir(path_name):
+def check_or_create_dir(path_name: str) -> None:
+    """
+    Helper function for checking if a folder exists and if it doesn't' to create it
+    """
     if not os.path.exists(path_name):
         os.makedirs(path_name)
 
