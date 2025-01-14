6cf5e1a781225199baddc9cc02784267ca750560
lilelinn <123090089+lilelinn@users.noreply.github.com>
2025-01-14 14:48:06 +0100

    chore: update documentation random_moves()

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -2,8 +2,11 @@ import random
 
 from classes import Vehicle, Board
 
-def random_moves(board: Board):
+def random_moves(board: Board) -> None:
+    """
+    Makes random moves until carter is in front of the exit and the game is finished.
+    """
     while not board.check_game_finished():
-        available_moves = board.check_available_moves()
+        available_moves: list[tuple[Vehicle, int]] = board.check_available_moves()
         vehicle, steps = random.choice(available_moves)
         board.move_vehicle(vehicle, steps)
