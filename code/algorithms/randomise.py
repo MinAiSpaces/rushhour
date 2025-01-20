2f92c0502d40d8c41f33bee8b37f36e03f81461f
Scovion <193891282+Scovion@users.noreply.github.com>
2025-01-20 11:32:48 +0100

    feat: added all_available_valid_finish_check function
    added algorithm that implements free_carter() in random_from_all_available_valid()

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -1,6 +1,7 @@
 import random
 
 from code.classes import Vehicle, Board
+from randomise import free_carter
 
 
 def random_from_all_available_valid(board: Board) -> None:
@@ -32,3 +33,15 @@ def random_vehicle_first(board: Board) -> None:
 
         board.move_vehicle(random_vehicle, steps)
 
+def all_available_valid_finish_check(board: Board) -> None:
+    """
+    Makes random moves until carter can finish the game.
+    Picks a move at random from all available valid moves unless moving carter finishes the game.
+    """
+    while not board.check_game_finished():
+        finish_game = free_carter(board)
+        if finish_game:
+            board.move_vehicle(board.vehicles['X'], finish_game)
+        else:
+            vehicle, steps = random.choice(board.check_available_moves())
+            board.move_vehicle(vehicle, steps)
