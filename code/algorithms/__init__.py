5f54e0238fe53ee565e4b37e3554aeccb1384dff
Scovion <193891282+Scovion@users.noreply.github.com>
2025-01-21 16:25:28 +0100

    feat: update __init__
    added StepRefiner to __init__.py

--- code/algorithms/__init__.py
+++ code/algorithms/__init__.py
@@ -1,5 +1,7 @@
 from .randomise import random_from_all_available_valid, random_vehicle_first, all_available_valid_finish_check, all_max_moves_finish_check
 from .heuristieken import check_useful_move
+from .steprefiner import StepRefiner
+
 
 
 __all__ = [
@@ -7,5 +9,6 @@ __all__ = [
     'random_vehicle_first',
     'all_available_valid_finish_check',
     'all_max_moves_finish_check',
-    'check_useful_move'
+    'check_useful_move',
+    'StepRefiner'
 ]
