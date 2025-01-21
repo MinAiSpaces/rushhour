196db3c510a0d5151df128507b06e16aaea0d597
Scovion <193891282+Scovion@users.noreply.github.com>
2025-01-21 10:12:22 +0100

    feat: create max_moves function
    max_moves returns only the maximum forward and backward movement of each vehicle.

--- code/algorithms/heuristieken.py
+++ code/algorithms/heuristieken.py
@@ -11,3 +11,19 @@ def free_carter(board: Board) -> int | None:
     # check if carter can move to finish
     if (carter.location[1][0] + available_moves) == (board.size - 1):
         return available_moves
+
+def all_max_moves(board: Board) -> list[tuple[Vehicle, int]]:
+    max_moves = []
+
+    for vehicle in board.vehicles.values():
+
+            # check forward movement
+            move_forwards = check_move_forwards(vehicle)
+            if board.move_forwards > 0:
+                 max_moves.append((vehicle, move_forwards))
+
+            move_backwards = board.check_move_backwards(vehicle)
+            if board.move_backwards > 0:
+                 max_moves.append((vehicle, -move_backwards))
+    print(max_moves)
+    return max_moves
