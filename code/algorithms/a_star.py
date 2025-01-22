c59ff2bafe55015b54a79a004b58ea601a26a07a
lilelinn <123090089+lilelinn@users.noreply.github.com>
2025-01-22 22:14:58 +0100

    feat: implement heuristic num_blocking_vehicles
    Added a new file a_star and implemented this heuristic to count the number of vehicles blocking carter's path

---
+++ code/algorithms/a_star.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+from code.classes import Board, Vehicle
+
+
+def num_blocking_vehicles(state: Board) -> int:
+    """
+    Counts the number of vehicles directly blocking Carter in front.
+    """
+    carter_row_num = state.vehicles['X'].start_row
+    carter_row: np.ndarray[object] = state.locations[carter_row_num]
+
+    idx_after_carter = state.vehicles['X'].location[-1][0] + 1
+
+    return len({carter_row[i] for i in range(idx_after_carter, len(carter_row)) if carter_row[i] != 0})
