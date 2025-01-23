6480a23c7f5f8035559d1fd40d144b9cfb0481a8
Scovion <193891282+Scovion@users.noreply.github.com>
2025-01-23 07:29:07 +0100

    feat: add run()
    add run method to StepRefiner

--- code/algorithms/steprefiner.py
+++ code/algorithms/steprefiner.py
@@ -6,12 +6,15 @@ from code.classes import Vehicle, Board
 
 
 class StepRefiner:
-    def __init__(self, board: Board, bins: int=10):
+    def __init__(self, board: Board, bin_size: int=20):
         if not board.check_game_finished():
             raise Exception("StepRefiner requires a solved board.")
 
         self.board = copy.deepcopy(board)
-        self.bins = bins
+        self.bin_size = bin_size
+        self.bins = len(self.board.steps) // self.bin_size
+        self.last_bin_size = len(self.board.steps) % self.bin_size
+
         self.rewind_steps = self.create_rewind_steps()
 
     def create_rewind_steps(self) -> list[tuple[str, int]]:
@@ -32,3 +35,21 @@ class StepRefiner:
             step = self.rewind_steps.pop()
             self.board.move_vehicle(self.board.vehicles[step[0]], step[1])
 
+    def run(self) -> Board:
+        """
+        Runs the StepRefiner.
+        """
+        new_steps = []
+
+        for bin in range(self.bins):
+            self.board.steps = []
+            old_state = self.board.locations
+            self.rewind_board(self.bin_size)
+            # BreadthFirst
+            # save steps (think of order)
+
+        self.board.steps = []
+        old_state = self.board.locations
+        self.rewind_board(self.last_bin_size)
+        # BreadthFirst
+        # save steps (think of order)
