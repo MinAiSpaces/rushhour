d6fb66033b24c279018d1610ef06d3774467da5f
lilelinn <123090089+lilelinn@users.noreply.github.com>
2025-01-23 23:36:05 +0100

    feat: implement another heuristic for A*
    Implemented num_two_blocking_vehicles() to count the number of vehicles directly blocking Carter in the front and the number of vehicles blocking these

--- code/algorithms/a_star.py
+++ code/algorithms/a_star.py
@@ -18,6 +18,27 @@ def num_blocking_vehicles(state: Board) -> int:
     return len({carter_row[i] for i in range(idx_after_carter, len(carter_row)) if carter_row[i] != 0})
 
 
+def num_two_blocking_vehicles(state: Board) -> int:
+    """
+    Counts the number of vehicles directly blocking Carter in the front and the
+    number of vehicles blocking these.
+    """
+    carter_row_num = state.vehicles['X'].start_row
+    carter_row: np.ndarray[object] = state.locations[carter_row_num]
+    idx_after_carter = state.vehicles['X'].location[-1][0] + 1
+
+    first_blocking_vehicles = {carter_row[i] for i in range(idx_after_carter, state.size) if carter_row[i] != 0}
+    second_blocking_vehicles = 0
+
+    for first_blocking_vehicle in first_blocking_vehicles:
+        if state.check_move_forwards(state.vehicles[first_blocking_vehicle]) == 0:
+            second_blocking_vehicles += 1
+        elif state.check_move_backwards(state.vehicles[first_blocking_vehicle]) == 0:
+            second_blocking_vehicles += 1
+
+    return len(first_blocking_vehicles) + second_blocking_vehicles
+
+
 class AStar:
     """
     This class explores Board configurations by performing a A* algorithm, starting
@@ -34,7 +55,7 @@ class AStar:
         self.queue = []
 
         # add the input board with its score and depth to the heap queue
-        heapq.heappush(self.queue, (num_blocking_vehicles(initial_state), 0, random.random(), initial_state))
+        heapq.heappush(self.queue, (num_two_blocking_vehicles(initial_state), 0, random.random(), initial_state))
 
         self.seen_states: set[tuple[tuple[object]]] = set()
         self.solution = None
@@ -60,7 +81,7 @@ class AStar:
                 self.seen_states.add(tuple(map(tuple, child_state.locations)))
 
                 # add the state with its score to the heap queue
-                score = depth + 1 + num_blocking_vehicles(child_state)
+                score = depth + 1 + num_two_blocking_vehicles(child_state)
                 heapq.heappush(self.queue, (score, depth + 1, random.random(), child_state))
 
     def run(self) -> None:
