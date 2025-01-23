857538769782874af3c462c24e73c983765324d8
lilelinn <123090089+lilelinn@users.noreply.github.com>
2025-01-23 22:27:11 +0100

    chore: updated documentation of a_star
    Wrote docstrings, updated comments, inserted missing type annotations and renamed variables

--- code/algorithms/a_star.py
+++ code/algorithms/a_star.py
@@ -20,21 +20,30 @@ def num_blocking_vehicles(state: Board) -> int:
 
 class AStar:
     """
-
+    This class explores Board configurations by performing a A* algorithm, starting
+    from an initial Board state. It maintains a priority queue of to be visisted
+    Board states and an archive of seen Board states. It generates child states for
+    valid moves and continues the search by selecting states with the lowest score
+    based on depth and heuristics until a solution is found.
     """
     def __init__(self, initial_state: Board) -> None:
         """
-
+        Initializes the A* algorithm with a specified Board state, setting up a
+        queue of Board states where the input Board serves as the initial state.
         """
         self.queue = []
+
+        # add the input board with its score and depth to the heap queue
         heapq.heappush(self.queue, (num_blocking_vehicles(initial_state), 0, random.random(), initial_state))
 
-        self.seen_states = set()
+        self.seen_states: set[tuple[tuple[object]]] = set()
         self.solution = None
 
     def build_children(self, next_state: Board, depth: int) -> None:
         """
-
+        Generates all possible child states from the picked Board state and adds them
+        to the heap queue of states if not seen earlier. Each child state represents
+        the Board configuration after a valid move by a Vehicle.
         """
         possible_moves: list[tuple[Vehicle, int]] = next_state.check_available_moves()
 
@@ -42,7 +51,7 @@ class AStar:
         for vehicle, steps in possible_moves:
             child_state = copy.deepcopy(next_state)
 
-            # make the valid move in the new board instance
+            # make the valid move in the state
             vehicle = child_state.vehicles[vehicle.name]
             child_state.move_vehicle(vehicle, steps)
 
@@ -50,20 +59,19 @@ class AStar:
             if tuple(map(tuple, child_state.locations)) not in self.seen_states:
                 self.seen_states.add(tuple(map(tuple, child_state.locations)))
 
-                total_score = depth + num_blocking_vehicles(child_state)
-
-                # add the state with its cost and heur to the heap queue
-                heapq.heappush(self.queue, (total_score, depth + 1, random.random(), child_state))
+                # add the state with its score to the heap queue
                 score = depth + 1 + num_blocking_vehicles(child_state)
+                heapq.heappush(self.queue, (score, depth + 1, random.random(), child_state))
 
     def run(self) -> None:
         """
-
+        Runs the algorithm until all possible Board states are visited or a solution
+        is found.
         """
         while self.queue:
 
-            # pop the state with the smallest cost and heur
-            total_score, depth, random_boundary, current_state = heapq.heappop(self.queue)
+            # pop the state with the lowest score; pick randomly if tied
+            score, depth, random_boundary, current_state = heapq.heappop(self.queue)
 
             if current_state.check_game_finished():
                 break
