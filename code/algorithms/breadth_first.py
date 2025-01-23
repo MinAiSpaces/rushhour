d401c0f34e36f27d9d3063fc2b4c3d26c3055a83
Scovion <193891282+Scovion@users.noreply.github.com>
2025-01-23 07:44:50 +0100

    feat: add finish to BreadthFirst run()
    added a finish parameter to run() in BreadthFirst to accomodate StepRefiner

--- code/algorithms/breadth_first.py
+++ code/algorithms/breadth_first.py
@@ -1,6 +1,8 @@
 import queue
 import copy
 
+import numpy as np
+
 from code.classes import Board, Vehicle
 
 
@@ -44,7 +46,7 @@ class BreadthFirst:
                 self.seen_states.add(tuple(map(tuple, child_state.locations)))
                 self.queue.put(child_state)
 
-    def run(self) -> None:
+    def run(self, finish: None | np.array = None) -> None:
         """
         Runs the algorithm until all possible Board states are visited or a solution
         is found.
@@ -52,8 +54,12 @@ class BreadthFirst:
         while not self.queue.empty():
             next_state = self.queue.get()
 
+            if finish:
+                if np.array_equal(next_state.locations, finish):
+                    break
+
             # stop if we find a solution
-            if next_state.check_game_finished():
+            else next_state.check_game_finished():
                 break
 
             self.build_children(next_state)
