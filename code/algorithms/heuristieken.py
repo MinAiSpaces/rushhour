72e0dbc54549b7482be72df6de1623af83d822de
Scovion <193891282+Scovion@users.noreply.github.com>
2025-01-24 10:29:58 +0100

    chore: add documentation to heuristieken.py

--- code/algorithms/heuristieken.py
+++ code/algorithms/heuristieken.py
@@ -4,7 +4,7 @@ from code.classes import Vehicle, Board
 
 def free_carter(board: Board) -> int | None:
     """
-    Check if the path for carter is free to end the game.
+    Checks if the path for carter is free to end the game.
     Return the steps if carter can reach the finish.
     """
     carter: Vehicle = board.vehicles['X']
@@ -16,16 +16,20 @@ def free_carter(board: Board) -> int | None:
 
 
 def all_max_moves(board: Board) -> list[tuple[Vehicle, int]]:
-    max_moves = []
+    """
+    Returns only the largest possible moves of all Vehicles on the board
+    as a list of moves.
+    """
+    max_moves: list[tuple[Vehicle, int]] = []
 
     for vehicle in board.vehicles.values():
 
-            # check forward movement
+            # check maximal forward movement
             move_forwards = board.check_move_forwards(vehicle)
             if move_forwards > 0:
                  max_moves.append((vehicle, move_forwards))
 
-            # check backward movement
+            # check maximal backward movement
             move_backwards = board.check_move_backwards(vehicle)
             if move_backwards > 0:
                  max_moves.append((vehicle, -move_backwards))
@@ -34,6 +38,10 @@ def all_max_moves(board: Board) -> list[tuple[Vehicle, int]]:
 
 
 def check_useful_move(board: Board, vehicle: Vehicle, steps: int) -> bool:
+    """
+    Checks for changes to the available moves after the move of 'vehicle' with
+    'steps' length is performed. Returns True if new moves become available.
+    """
     new_moves: list[tuple[Vehicle, int]] = []
     check_board = copy.deepcopy(board)
 
@@ -41,7 +49,10 @@ def check_useful_move(board: Board, vehicle: Vehicle, steps: int) -> bool:
     check_board.move_vehicle(vehicle, steps)
     moves_after = check_board.check_available_moves()
 
+    # find the differnce in available moves
     differences = list(set(moves_after) - set(moves_before))
+
+     # remove the moved vehicle from differences
     for move in differences:
          if move[0] != vehicle:
               new_moves.append(move)
