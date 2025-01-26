2c6557d13621230587e73d8a5139e37135006d13
MinAiSpaces <193891364+MinAiSpaces@users.noreply.github.com>
2025-01-26 19:46:03 +0100

    bug: get DFS to work with new code structure

--- code/algorithms/depth_first.py
+++ code/algorithms/depth_first.py
@@ -1,6 +1,6 @@
 import copy
 
-from code.classes import Board, Vehicle
+from code.classes import Board, Mover, Game
 
 
 class DepthFirst:
@@ -16,30 +16,32 @@ class DepthFirst:
         setting up a stack of Board states where the input Board serves as the initial
         state.
         """
-        self.states = [copy.deepcopy(initial_state)]
+        self.states = [(copy.deepcopy(initial_state), [])]
         self.seen_states: set[tuple[tuple[object]]] = set()
         self.solution = None
+        self.moves: list[tuple[str, int]] = []
 
-    def build_children(self, next_state: Board) -> None:
+    def build_children(self, next_state: Board, move_history: list[tuple[str, int]]) -> None:
         """
         Generates all possible child states from the picked Board state and appends
         them to the stack of states if not seen earlier. Each child state represents
         the Board configuration after a valid move by a Vehicle.
         """
-        possible_moves: list[tuple[Vehicle, int]] = next_state.check_available_moves()
+        mover = Mover(next_state)
+        possible_moves: list[tuple[str, int]] = mover.get_all_available_moves()
 
         # add a new board instance to the stack for each valid move
-        for vehicle, steps in possible_moves:
+        for move in possible_moves:
             child_state = copy.deepcopy(next_state)
+            new_mover = Mover(child_state)
 
             # make the valid move in the new board instance
-            vehicle = child_state.vehicles[vehicle.name]
-            child_state.move_vehicle(vehicle, steps)
+            new_mover.move_vehicle(move)
 
             # https://www.geeksforgeeks.org/how-to-fix-the-typeerror-unhashable-type-numpy-ndarray/
             if tuple(map(tuple, child_state.locations)) not in self.seen_states:
                 self.seen_states.add(tuple(map(tuple, child_state.locations)))
-                self.states.append(child_state)
+                self.states.append((child_state, move_history + [move]))
 
     def run(self) -> None:
         """
@@ -49,12 +51,14 @@ class DepthFirst:
         while self.states:
 
             # get the board instance on top of the stack
-            next_state = self.states.pop()
+            next_state, move_history = self.states.pop()
 
             # stop if we find a solution
-            if next_state.check_game_finished():
+            if Game.is_finished(next_state):
+                self.solution = next_state
+                self.moves = move_history
                 break
 
-            self.build_children(next_state)
+            self.build_children(next_state, move_history)
 
         self.solution = next_state
